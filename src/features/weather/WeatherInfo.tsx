62ca435ed1b34185b234cb1a736601abdfa54f72
Mariam Chubinidze <Mchubinidze@crocobet.com>
2023-02-09 17:31:33 +0400

    grid responsive

--- src/features/weather/WeatherInfo.tsx
+++ src/features/weather/WeatherInfo.tsx
@@ -3,22 +3,17 @@ import { ICoords } from "../location/locationTypes";
 import { useWeatherInfo } from "./useWeatherInfo";
 import WeatherComponent from "./WeatherComponent";
 import WeatherHeader from "./WeatherHeader";
-
-const additionalInfos = {
-    display: 'grid',
-    gridTemplateColumns: "1fr 1fr 1fr",
-    gridGap: 10,
- }
+import './css/WeatherInfo.css'
 
 const WeatherInfo : FC<{location: ICoords}> = ({location}) => {
     const info = useWeatherInfo(location);
 
     return (   
         <>
-            {info != undefined && <div>
+            {info != undefined && <div className="weather-details">
                 <WeatherHeader location={info.location} weather={info.weather} temp={info.temp} feels_like={info.feels_like}></WeatherHeader>
                 
-                <div style={additionalInfos}>
+                <div className="additional-infos">
                     <WeatherComponent title = "Humidity" value={info.humidity + "%"} imgSrc="./src/assets/humidity.png"></WeatherComponent>
                     <WeatherComponent title = "Wind speed" value={info.wind_speed + " m/s"} imgSrc="./src/assets/wind.png"></WeatherComponent>
                     <WeatherComponent title = "Visibility" value={info.visibility + " m"} imgSrc="./src/assets/visibility.png"></WeatherComponent>
